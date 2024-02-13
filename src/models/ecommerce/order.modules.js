c25536c7dd4db72fe5d19a22937ebeb6e9a522ff
CoderSid063 <sudhansupanda063@gmail.com>
2024-02-13 11:00:02 +0530

    connect to mongo Atlas

---
+++ src/models/ecommerce/order.modules.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+const orderItemSchema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'product',
+  },
+  quantity: {
+    type: Number,
+    requird: true,
+  },
+});
+
+const orderSchema = new mongoose.Schema(
+  {
+    orderPrice: {
+      type: Number,
+      required: true,
+    },
+    customer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    orderItems: {
+      type: [orderItemSchema],
+    },
+    adderss: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['PENDING', 'CANCELLED', 'DELIVERED'],
+      default: 'PENDING',
+    },
+  },
+  { timestamps: true }
+);
+
+export const Order = mongoose.model('Order', orderSchema);
