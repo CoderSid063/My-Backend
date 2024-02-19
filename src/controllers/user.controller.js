d64f53c9eba92237a308dc1bdee30594450ef6ab
CoderSid063 <sudhansupanda063@gmail.com>
2024-02-19 18:38:58 +0530

    chor : all setup configured

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,4 +6,25 @@ const registerUser = asyncHandler(async (req, res) => {
   });
 });
 
+//this function not using "asyncHandler" wrapper.
+
+/* 
+if we have no function that need to use "asyn await" then we can't use asyn await method in the below "registerUser",when i used await in ("fetchUserData") without creating  it show the error :- "ReferenceError: fetchUserData is not defined" .
+*/
+
+/*
+const registerUser = async (req, res) => {
+  try {
+    // const userData = await fetchUserData(req.userId);
+
+    res.status(200).json({
+      message: "my Backend",
+    });
+  } catch (error) {
+    console.log("Error in registerUser:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+*/
+
 export default registerUser;
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,15 +1,23 @@
+/*
+HigherOrder function :- a higher order function which take function as a parameter or can return the function
+Ex :--  const asyncHandler = (fn) => () =>{}
+        const asyncHandler = (fn) => {() =>{}}
+        const asyncHandler = (fn) => {async()=>{}};
+*/
+
+// This is a higher order function which return a promice. this is used in a production company.
+
+/*
 const asyncHandler = (requestHandler) => {
   return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
   };
 };
+*/
 
-export { asyncHandler };
+// This is a higher order function using try catch block
 
-// const asyncHandler = (fn) => {() =>{}}
-// const asyncHandler = (fn) => {async()=>{}};
-
-/*const asyncHandler = (fn) => async (req, res, next) => {
+const asyncHandler = (fn) => async (req, res, next) => {
   try {
     await fn(res, res, next);
   } catch (error) {
@@ -19,4 +27,5 @@ export { asyncHandler };
     });
   }
 };
-*/
+
+export { asyncHandler };
